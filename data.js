25b3439879f271ceb0ff1082f975edd65a335d3b
Lorenzo De Simone <lorenzo.desimone2007@gmail.com>
2025-05-15 22:00:10 +0200

    data json e javascript

---
+++ data.js
@@ -0,0 +1,112 @@
+const moviesData = [
+    {
+        id: "designated",
+        title: "Designated Survivor",
+        description: "Un membro del gabinetto diventa improvvisamente Presidente degli Stati Uniti dopo un attacco che uccide la maggior parte del governo. L'uomo deve imparare rapidamente il nuovo lavoro e a lottare contro gli orrori futuri.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/designeted.jpg",
+        year: "2025",
+        duration: "1h 54m",
+        regia: "Lorenzo De Simone",
+        cast: "Lorenzo De Simone, Davide Mariani",
+        genere: "Thriller, Horror, Drammatico"
+    },
+    {
+        id: "lacasa",
+        title: "La Casa de Papel",
+        description: "Un enigmatico professore recluta otto persone con abilità uniche per rapinare la Zecca di Stato spagnola, dando vita a un elaborato piano che sfida la polizia e l'opinione pubblica.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/lacasa.jpg",
+        year: "2017",
+        duration: "1h 50m",
+        regia: "Álex Pina",
+        cast: "Álvaro Morte, Úrsula Corberó, Pedro Alonso",
+        genere: "Azione, Crimine, Dramma"
+    },
+    {
+        id: "lucifer",
+        title: "Lucifer",
+        description: "Lucifero lascia l'Inferno per vivere a Los Angeles e gestire un nightclub, ma presto diventa consulente della polizia.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/lucifer.jpg",
+        year: "2020",
+        duration: "2h 00m",
+        regia: "Tom Kapinos",
+        cast: "Tom Ellis, Lauren German",
+        genere: "Crime, Drama, Fantasy"
+    },
+    {
+        id: "laregina",
+        title: "La Regina degli Scacchi",
+        description: "Beth Harmon, un'orfana prodigio degli scacchi, lotta contro le dipendenze mentre cerca di diventare la miglior scacchista del mondo.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/laregina.jpg",
+        year: "2020",
+        duration: "1h 45m",
+        regia: "Scott Frank",
+        cast: "Anya Taylor-Joy, Bill Camp, Marielle Heller",
+        genere: "Drammatico, Biografico"
+    },
+    {
+        id: "outerbanks",
+        title: "Outer Banks",
+        description: "Un gruppo di adolescenti della Carolina del Nord scopre una mappa del tesoro che svela un segreto di famiglia e dà il via a una pericolosa avventura.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/outerbanks.jpg",
+        year: "2020",
+        duration: "1h 40m",
+        regia: "Shannon Burke, Jonas Pate",
+        cast: "Chase Stokes, Madelyn Cline, Madison Bailey",
+        genere: "Azione, Avventura, Dramma"
+    },
+    {
+        id: "peaky",
+        title: "Peaky Blinders",
+        description: "Nel dopoguerra, la famiglia Shelby guida una gang criminale a Birmingham, espandendo la propria influenza in un mondo di violenza e politica.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/peaky.jpg",
+        year: "2013",
+        duration: "1h 55m",
+        regia: "Steven Knight",
+        cast: "Cillian Murphy, Paul Anderson, Helen McCrory",
+        genere: "Crimine, Dramma, Storico"
+    },
+    {
+        id: "prison",
+        title: "Prison Break",
+        description: "Un uomo entra deliberatamente in prigione per salvare il fratello condannato a morte, dando il via a un complesso piano di fuga.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/prison.jpg",
+        year: "2005",
+        duration: "2h 10m",
+        regia: "Paul Scheuring",
+        cast: "Wentworth Miller, Dominic Purcell, Sarah Wayne Callies",
+        genere: "Azione, Crimine, Dramma"
+    },
+    {
+        id: "squidgame",
+        title: "Squid Game",
+        description: "Centinaia di persone indebitate partecipano a giochi mortali per vincere un premio in denaro, ma ogni prova potrebbe essere l’ultima.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/squidgame.jpg",
+        year: "2021",
+        duration: "1h 55m",
+        regia: "Hwang Dong-hyuk",
+        cast: "Lee Jung-jae, Park Hae-soo, HoYeon Jung",
+        genere: "Thriller, Drammatico, Sopravvivenza"
+    },
+    {
+        id: "strangerthings",
+        title: "Stranger Things",
+        description: "Un ragazzo scompare in una cittadina dell'Indiana, mentre i suoi amici scoprono una ragazza con poteri paranormali e un mondo oscuro nascosto.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/strangerthings.jpg",
+        year: "2016",
+        duration: "2h 00m",
+        regia: "Duffer Brothers",
+        cast: "Millie Bobby Brown, Finn Wolfhard, Winona Ryder",
+        genere: "Fantascienza, Horror, Drammatico"
+    },
+    {
+        id: "suits",
+        title: "Suits",
+        description: "Mike Ross, un brillante truffatore con una memoria fotografica, viene assunto da un avvocato di successo nonostante non abbia una laurea in legge.",
+        imgSrc: "./IMG/IMG-MOVIES-ORIZZONTALI/suits.jpg",
+        year: "2011",
+        duration: "1h 50m",
+        regia: "Aaron Korsh",
+        cast: "Gabriel Macht, Patrick J. Adams, Meghan Markle",
+        genere: "Dramma, Legale"
+    }
+];
